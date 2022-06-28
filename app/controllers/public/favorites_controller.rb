6726f4b8e012f1d8fced8e717520eb27dccd7d24
shikesesame <shikekosakilove@gmail.com>
2022-06-28 11:31:22 +0000

    Add Commit

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,14 +3,14 @@ class Public::FavoritesController < ApplicationController
         @recipe = Recipe.find(params[:recipe_id])
         @favorite = current_customer.favorites.new(recipe_id: @recipe.id)
         @favorite.save
-        redirect_to public_recipe_path(@recipe)
+        redirect_back(fallback_location: root_path)
       end
     
       def destroy
         @recipe = Recipe.find(params[:recipe_id])
         @favorite = current_customer.favorites.find_by(recipe_id: @recipe.id)
         @favorite.destroy
-        redirect_to public_recipe_path(@recipe)
+        redirect_back(fallback_location: root_path)
       end
 
 end
